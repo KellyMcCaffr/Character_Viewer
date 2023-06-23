d73c5e2f4d93f42fb450d9c4b3ea5f9221af3fa5
kellymccaffr <McCaffreyk19@mail.wlu.edu>
2023-06-23 15:46:01 -0700

    First commit

---
+++ app/src/main/java/com/sample/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.e("344r","Here is the data api field: " + BuildConfig.data_api)
+    }
+}
